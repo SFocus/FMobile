9d1c8dbc86e218f20a43f7391f419dea27a2e766
SFocus <svyatoslav472@gmail.com>
2016-03-12 21:56:43 +0200

    Internet Permision

--- FMobile/src/main/java/com/androidbelieve/drawerwithswipetabs/PrimaryFragment.java
+++ FMobile/src/main/java/com/androidbelieve/drawerwithswipetabs/PrimaryFragment.java
@@ -17,4 +17,5 @@ public class PrimaryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.primary_layout,null);
     }
+
 }
