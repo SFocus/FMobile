15bbf4d81799d5e01c0ddb518da6daadf082ea93
Andrew <self1995@mail.ua>
2016-03-13 13:10:49 +0200

    Debugging click listener

--- FMobile/src/main/java/com/androidbelieve/drawerwithswipetabs/SentFragment.java
+++ FMobile/src/main/java/com/androidbelieve/drawerwithswipetabs/SentFragment.java
@@ -6,15 +6,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Ratan on 7/29/2015.
  */
-public class SentFragment extends Fragment {
+public class SentFragment extends Fragment implements View.OnClickListener{
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        container.findViewById(R.id.search_btn).setOnClickListener(this);
+
         return inflater.inflate(R.layout.sent_layout,null);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.search_btn:
+                WebParser.DataSource.getQuickSearchResult("test");
+                break;
+        }
     }
 }
