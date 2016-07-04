ce193faf07ef782af7d12eb4b1437621505fc3f9
SFocus <svyatoslav472@gmail.com>
2016-07-04 18:13:48 +0300

    LoadFilms

--- FMobile/src/main/java/com/androidbelieve/drawerwithswipetabs/PrimaryFragment.java
+++ FMobile/src/main/java/com/androidbelieve/drawerwithswipetabs/PrimaryFragment.java
@@ -1,17 +1,21 @@
 package com.androidbelieve.drawerwithswipetabs;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 
 import org.jsoup.nodes.Document;
 
@@ -31,32 +35,49 @@ public class PrimaryFragment extends Fragment {
     private List<VideoItem> filmList = new ArrayList<>();
     private List<VideoItem> filmList2 = new ArrayList<>();
     private VideoAdapter mAdapter;
-    private Toolbar toolbar;
-
-    private int page = 1;
-
+    private int  page = 0;
+    int totalItemCount, visibleItemCount, firstVisibleItem;
+    boolean loading = false;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.primary_layout, null);
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycleView);
         recyclerView.setHasFixedSize(true);
-        GridLayoutManager llm = new GridLayoutManager(getActivity(), 2, GridLayoutManager.VERTICAL, false);
+        final GridLayoutManager llm = new GridLayoutManager(getActivity(), 2, GridLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(llm);
         mAdapter = new VideoAdapter(filmList);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(mAdapter);
-        toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+
+        recyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                int totalItemCount = llm.getItemCount();
+                int visibleItemCount = llm.getChildCount();
+                int firstVisibleItem = llm.findFirstVisibleItemPosition();
+                Log.d("totalItemCount", Integer.toString(totalItemCount));
+                Log.d("visible", Integer.toString(visibleItemCount));
+                Log.d("firstv", Integer.toString(firstVisibleItem));
+
+                if (totalItemCount == visibleItemCount + firstVisibleItem) {
+                   loading=true;
+                }
+                if(loading)
+                {
+                    loading = false;
+                    page++;
+                    new LoadFilms().execute();
+                }
+            }
+            });
 
         new LoadFilms().execute();
-        addDate();
         return view;
     }
 
-    private void addDate(){
-        filmList.clear();
-
-    }
 
     private class LoadFilms extends AsyncTask<String, Void, Document>
     {
@@ -69,11 +90,11 @@ public class PrimaryFragment extends Fragment {
         @Override
         protected void onPostExecute(Document document) {
             super.onPostExecute(document);
+            filmList2.clear();
             filmList2 = new PageParser(document).getFilms();
             for(int i=0; i<filmList2.size();i++)
             {
                 filmList.add(filmList2.get(i));
-                Log.d("dfddfgg",filmList2.get(i).getLink());
             }
             mAdapter.notifyDataSetChanged();
         }
