a9c4c9f795448f6adda79cea45e6cb99f7e5e6f3
Andrew <self1995@mail.ua>
2016-07-07 21:28:53 +0300

    Improved VideoEntry

--- FMobile/src/main/java/models/VideoEntry.java
+++ FMobile/src/main/java/models/VideoEntry.java
@@ -1,5 +1,9 @@
 package models;
 
+
+
+import org.jsoup.helper.StringUtil;
+
 import java.util.List;
 
 /**
@@ -10,18 +14,21 @@ public class VideoEntry {
     /**
      * Entry information selectors
      */
-    public static final String ENTRY_NAME_SELECTOR = ".b-tab-item__title-inner span";
-    public static final String ENTRY_NAME_ALT_SELECTOR = ".b-tab-item__title-inner div";
-    public static final String ENTRY_IMAGES_SET_SELECTOR = ".images-show[style]";
-    public static final String ENTRY_DESCRIPTION_SELECTOR = ".item-decription";
-    public static final String ENTRY_POSITIVE_VOTES_SELECTOR = ".m-tab-item__vote-value_type_yes";
-    public static final String ENTRY_NEGATIVE_VOTES_SELECTOR = ".m-tab-item__vote-value_type_no";
-    public static final String ENTRY_INFO_CELL_SELECTOR = ".m-item-info-td_type-short";
-    public static final String ENTRY_GENRES_SELECTOR = "span > a";
-    public static final String ENTRY_YEAR_SELECTOR = "a";
-    public static final String ENTRY_COUNTRY_SELECTOR = "a";
-    public static final String ENTRY_DIRECTOS_SELECTOR = "span > a";
-    public static final String ENTRY_CASTS_SELECTOR = "span > a";
+    public static final String ENTRY_NAME_SELECTOR              = ".b-tab-item__title-inner span";
+    public static final String ENTRY_NAME_ALT_SELECTOR          = ".b-tab-item__title-inner div";
+    public static final String ENTRY_IMAGES_SET_SELECTOR        = ".images-show[style]";
+    public static final String ENTRY_DESCRIPTION_SELECTOR       = ".item-decription";
+    public static final String ENTRY_POSITIVE_VOTES_SELECTOR    = ".m-tab-item__vote-value_type_yes";
+    public static final String ENTRY_NEGATIVE_VOTES_SELECTOR    = ".m-tab-item__vote-value_type_no";
+    public static final String ENTRY_INFO_CELL_SELECTOR         = ".m-item-info-td_type-short";
+    public static final String ENTRY_GENRES_SELECTOR            = "span > a";
+    public static final String ENTRY_YEAR_SELECTOR              = "a";
+    public static final String ENTRY_COUNTRY_SELECTOR           = "a";
+    public static final String ENTRY_DIRECTOS_SELECTOR          = "span > a";
+    public static final String ENTRY_CASTS_SELECTOR             = "span > a";
+
+    public static final String SPACE_SEPARATOR                  = " ";
+    public static final String COMMA_SEPARATOR                  =", ";
 
     private List<String> genres;
     private List<String> year;
@@ -38,21 +45,21 @@ public class VideoEntry {
 
 
     public VideoEntry(List<String> genres, List<String> year, List<String> countries, List<String> directors, List<String> casts, List<String> images, String name, String altName, String positiveVotes, String negativeVotes, String description) {
-        this.genres = genres;
-        this.year = year;
-        this.countries = countries;
-        this.directors = directors;
-        this.casts = casts;
-        this.images = images;
-        this.name = name;
-        this.altName = altName;
-        this.positiveVotes = positiveVotes;
-        this.negativeVotes = negativeVotes;
-        this.description = description;
+        this.genres         = genres;
+        this.year           = year;
+        this.countries      = countries;
+        this.directors      = directors;
+        this.casts          = casts;
+        this.images         = images;
+        this.name           = name;
+        this.altName        = altName;
+        this.positiveVotes  = positiveVotes;
+        this.negativeVotes  = negativeVotes;
+        this.description    = description;
     }
 
-    public List<String> getGenres() {
-        return genres;
+    public String getGenres(String separator) {
+        return StringUtil.join(genres, separator);
     }
 
     public List<String> getYear() {
@@ -63,12 +70,12 @@ public class VideoEntry {
         return countries;
     }
 
-    public List<String> getDirectors() {
-        return directors;
+    public String getDirectors(String separator) {
+        return StringUtil.join(directors, separator);
     }
 
-    public List<String> getCasts() {
-        return casts;
+    public String getCasts(String separator) {
+        return StringUtil.join(casts, separator);
     }
 
     public List<String> getImages() {
