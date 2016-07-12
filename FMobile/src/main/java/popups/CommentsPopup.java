2b986e2bd02606fe605791f141ec490ddd6da1f8
SFocus <svyatoslav472@gmail.com>
2016-07-12 19:03:44 +0300

    Design modal background

--- FMobile/src/main/java/popups/CommentsPopup.java
+++ FMobile/src/main/java/popups/CommentsPopup.java
@@ -3,6 +3,7 @@ package popups;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.WindowManager;
 
 import com.androidbelieve.drawerwithswipetabs.R;
 
@@ -24,6 +25,12 @@ public class CommentsPopup extends Activity{
                 ( int ) ( dm.widthPixels * 0.8 ),
                 ( int ) ( dm.heightPixels * 0.8 )
         );
+
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.dimAmount = 0.6f; // уровень затемнения от 1.0 до 0.0
+        getWindow().setAttributes(lp);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
     }
 
 }
