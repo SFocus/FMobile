139b2dd5f68389a549d1902488af4bbfce89ba0c
Andrew <self1995@mail.ua>
2016-07-24 10:56:29 +0300

    Added params encoder

--- FMobile/src/main/java/WebParser/QueryBuilder.java
+++ FMobile/src/main/java/WebParser/QueryBuilder.java
@@ -1,17 +1,47 @@
 package WebParser;
 
+import java.net.URLEncoder;
+
 /**
  * Created by Andrew on 04.07.2016.
  */
 public class QueryBuilder {
 
+    /*
+    *  Default charset
+    */
+    private static final String defaultCharset = "UTF-8";
+
+    /**
+     * @param in    url data
+     * @return      Encoded data
+     */
+    private static Object[] encodeParams(Object[] in)
+    {
+        try {
+            Object[] out = new Object[in.length];
+            for(int i = 0; i < in.length; i++)
+                out[i] = URLEncoder.encode(in[i].toString(), defaultCharset);
+
+            return out;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return in;
+        }
+
+    }
     /**
      * @param in   input url which will be replaced with given data
      * @param data url data
      * @return replaced url with given data
      */
     private static String replace(String in, Object[] data) {
-        return String.format(in, data);
+        return String.format(
+                in,
+                encodeParams(data)
+        );
     }
 
     /**
